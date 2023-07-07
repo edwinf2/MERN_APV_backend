301a32c79cfcf3a5d763a256982d7e80ee44183a
Edwin Figueroa <edwinprogramador@gmail.com>
2023-07-07 16:31:35 -0600

    cambios en index.js 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import conectarDB from "./config/db.js";
@@ -13,8 +13,8 @@ conectarDB();
 const dominiosPermitidos = [process.env.FRONTEND_URL];
 
 const corsOptions = {
-    origin: function(origin, callback) {
-        if (dominiosPermitidos.indexOf(origin)!== -1) {
+    origin: function (origin, callback) {
+        if (dominiosPermitidos.indexOf(origin) !== -1) {
             // El origen del request esta permitidos
             callback(null, true);
         } else {
@@ -24,8 +24,8 @@ const corsOptions = {
 };
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL
-   }));
+    origin: '*'
+}));
 
 app.use("/api/veterinarios", veterinarioRouters);
 app.use("/api/pacientes", pacientesRouters);
